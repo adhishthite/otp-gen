569df91a3857f3549fb425be0857bfa9ca851e6c
adhishthite <adhish.thite@gmail.com>
2023-12-17 12:07:41 +0530

    modified OTP generation

--- app/core/generator.py
+++ app/core/generator.py
@@ -1,36 +1,153 @@
 """
 (c) Adhish Thite
 
-This module contains functions to generate OTPs
+This module, generator.py, contains the OTPGenerator class for generating One-Time Passwords (OTPs).
 
-generator.py
+The OTPGenerator class offers functionalities to create customizable OTPs which can be numeric or alphanumeric,
+with a specified length and time-to-live (TTL).
+The generated OTPs come along with metadata and information about their creation and expiration.
 """
 
 import secrets
 import string
+import uuid
+import time
 
 
 class OTPGenerator:
     """
-    This class contains functions to generate OTPs
+    A class for generating One-Time Passwords (OTPs).
+
+    This class allows the generation of OTPs which can be numeric or alphanumeric, with a specified length and TTL.
+    It also provides information about the generated OTP, such as creation time, expiration time, and TTL.
+    Additionally, it generates a unique meta identifier for each OTP.
+
+    Attributes:
+        __length (int): The length of the OTP. Default is 6 characters.
+        __alphanumeric (bool): A flag to determine if the OTP should be alphanumeric. Defaults to False (numeric only).
+        __kwargs (dict): Additional keyword arguments.
+        __meta (dict): Metadata associated with the OTP. Defaults to an empty dictionary.
+        __ttl (int): Time-to-live in seconds for the OTP. Defaults to 3600 seconds (1 hour).
+        __otp (str, None): The generated OTP. None until an OTP is generated.
+        __otp_info (dict, None): Information about the generated OTP. None until an OTP is generated.
+
+    Methods:
+        generate_otp(): Generates a random OTP based on the specified length and alphanumeric flag.
+        generate_meta(): Generates a random meta identifier for the OTP.
+        get_otp(): Returns the generated OTP along with its information and metadata.
+
+    Example:
+        >>> otp_gen = OTPGenerator(length=4, alphanumeric=True)
+        >>> otp_info = otp_gen.get_otp()
+        >>> print(otp_info)
     """
 
-    def __init__(self, meta: dict = None, length: int = 6, alphanumeric: bool = False, ttl: int = 3600, **kwargs):
-        self.length = length
-        self.alphanumeric = alphanumeric
-        self.kwargs = kwargs
-        self.meta = meta or {}
+    def __init__(
+        self,
+        meta: dict = None,
+        length: int = 6,
+        alphanumeric: bool = False,
+        ttl: int = 3600,
+        generate: bool = True,
+        **kwargs
+    ):
 
-    def generate_otp(self) -> str:
         """
-        Generate a random OTP
+        Constructs all the necessary attributes for the OTPGenerator object.
 
-        :return: OTP (str)
+        Parameters:
+            meta (dict): Metadata associated with the OTP. Defaults to None, which initializes to an empty dictionary.
+            length (int): The length of the OTP. Default is 6 characters.
+            alphanumeric (bool): A flag to determine if the OTP should be alphanumeric. Defaults to False (numeric only).
+            ttl (int): Time-to-live in seconds for the OTP. Defaults to 3600 seconds (1 hour).
+            generate (bool): A flag to determine if an OTP should be generated upon instantiation. Defaults to True.
+            **kwargs: Additional keyword arguments.
+        """
+        # Validate length
+        if not isinstance(length, int) or length <= 0:
+            raise ValueError("Length must be a positive integer.")
+
+        # Validate alphanumeric
+        if not isinstance(alphanumeric, bool):
+            raise ValueError("Alphanumeric must be a boolean value.")
+
+        # Validate TTL
+        if not isinstance(ttl, int) or ttl < 0 or ttl > 86400:  # 86400 seconds = 24 hours
+            raise ValueError("TTL must be a non-negative integer, up to 86400 (24 hours).")
+
+        # Validate meta (optional)
+        if meta is not None and not isinstance(meta, dict):
+            raise ValueError("Meta must be a dictionary.")
+
+        self.__length = length
+        self.__alphanumeric = alphanumeric
+        self.__kwargs = kwargs
+        self.__meta = meta or {}
+        self.__ttl = ttl
+        self.__otp = None
+        self.__otp_info = None
+        self.otp_dict = None
+
+        # Generate OTP and meta
+        if generate:
+            self.otp_dict = self.get_otp()
+
+    def generate_otp(self) -> None:
+        """
+        Generate a random OTP.
+
+        This method creates a random OTP based on the specified length and alphanumeric flag.
+        It also records the creation time, expiration time, and TTL of the OTP.
+
+        Returns:
+            None: This method updates the class attributes __otp and __otp_info in place.
         """
 
-        if self.alphanumeric:
+        if self.__alphanumeric:
             alphabet = string.ascii_letters + string.digits
         else:
             alphabet = string.digits
 
-        return "".join(secrets.choice(alphabet) for _ in range(self.length))
+        self.__otp = "".join(secrets.choice(alphabet) for _ in range(self.__length))
+
+        creation_time = time.time()
+        expiration_time = creation_time + self.__ttl
+
+        self.__otp_info = {
+            "length": self.__length,
+            "alphanumeric": self.__alphanumeric,
+            "creation_time": creation_time,
+            "expiration_time": expiration_time,
+            "ttl": self.__ttl,
+        }
+
+    def generate_meta(self) -> None:
+        """
+        Generate a random meta identifier for the OTP.
+
+        This method creates a unique meta identifier using UUID (Universally Unique Identifier).
+
+        Returns:
+            None: This method updates the class attribute __meta in place.
+        """
+
+        self.__meta = {
+            "id": str(uuid.uuid4()),
+        }
+
+    def get_otp(self) -> dict:
+        """
+        Get the generated OTP along with its information and metadata.
+
+        This method returns the OTP, its information (such as creation and expiration time, TTL),
+        and its metadata (including a unique identifier).
+
+        Returns:
+            dict: A dictionary containing the OTP, OTP information, and metadata.
+        """
+        self.generate_otp()
+
+        if not self.__meta:
+            self.generate_meta()
+
+        return {"otp": self.__otp, "otp_info": self.__otp_info, "meta": self.__meta}
--- tests/core/test_generator.py
+++ tests/core/test_generator.py
@@ -20,7 +20,6 @@ class TestOTPGenerator:
         (8, False, 8, 'isnumeric'),
         (None, True, 6, 'isalnum'),
         (8, True, 8, 'isalnum'),
-        (100, None, 100, 'isnumeric'),
         (100, False, 100, 'isalnum'),
     ])
     def test_generate_otp(self, length, alphanumeric, length_expected, check_method):
@@ -28,9 +27,46 @@ class TestOTPGenerator:
         Parametrized test for the generate_otp() function.
         """
         if length is None:
-            otp = OTPGenerator(alphanumeric=alphanumeric).generate_otp()
+            otp = OTPGenerator(alphanumeric=alphanumeric).otp_dict['otp']
         else:
-            otp = OTPGenerator(length=length, alphanumeric=alphanumeric).generate_otp()
+            otp = OTPGenerator(length=length, alphanumeric=alphanumeric).otp_dict['otp']
 
         assert len(otp) == length_expected
         assert getattr(otp, check_method)()
+
+    def test_generate_meta(self):
+        otp_gen1 = OTPGenerator()
+        otp_gen2 = OTPGenerator()
+
+        meta1 = otp_gen1.otp_dict['meta']
+        meta2 = otp_gen2.otp_dict['meta']
+
+        assert 'id' in meta1 and 'id' in meta2
+        assert meta1['id'] != meta2['id']
+
+    def test_ttl_functionality(self):
+        ttl = 120
+        otp_gen = OTPGenerator(ttl=ttl)
+        otp_info = otp_gen.otp_dict['otp_info']
+
+        assert otp_info['ttl'] == ttl
+        assert otp_info['expiration_time'] - otp_info['creation_time'] == ttl
+
+    @pytest.mark.parametrize("invalid_input", [
+        (-1, False),
+        ('invalid', False),
+        (None, 'invalid')
+    ])
+    def test_invalid_input_handling(self, invalid_input):
+        with pytest.raises(ValueError):
+            OTPGenerator(*invalid_input)
+
+    def test_metadata_integrity(self):
+        otp_gen = OTPGenerator()
+        otp_info = otp_gen.otp_dict['otp_info']
+        meta = otp_gen.otp_dict['meta']
+
+        assert 'creation_time' in otp_info
+        assert 'id' in meta
+        assert isinstance(otp_info['creation_time'], float)
+        assert isinstance(meta['id'], str)
